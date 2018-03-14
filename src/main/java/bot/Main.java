b9cf3375c1b63077e614d23be9366b6d1757dcd0
Dmitry <elegiacal.boy@gmail.com>
2018-03-14 16:57:59 +0200

    simple echo bot with logs+webhook

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -5,7 +5,9 @@ package bot;
 import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.impl.UpdatesHandler;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SetWebhook;
 import com.pengrad.telegrambot.response.BaseResponse;
@@ -21,6 +23,42 @@ public class Main {
 
     public static void main(String[] args) {
         final TelegramBot bot = new TelegramBot.Builder("521960020:AAHGQKGlW_QbuedUGikDDcfagOMqbMYguI4").okHttpClient(new OkHttpClient()).build();
+        UpdatesHandler uh = new UpdatesHandler(1);
+        uh.start(bot,new UpdatesListener() {
+            public int process(List<Update> list) {
+                for(final Update item: list){
+                    try
+                    {
+                        FileWriter writer = new FileWriter("./logs/mainLogFile.txt", true);
+                        BufferedWriter bufferWriter = new BufferedWriter(writer);
+                        bufferWriter.write("Date: "+item.message().date() +" text:"+item.message().text()+" chat:"+item.message().chat()+"\n");
+                        bufferWriter.close();
+                        SendMessage sm = new SendMessage(item.message().chat().id(), item.message().text());
+                        bot.execute(sm, new Callback<SendMessage, SendResponse>() {
+                            public void onResponse(SendMessage sendMessage, SendResponse sendResponse) {
+
+                            }
+
+                            public void onFailure(SendMessage sendMessage, IOException e) {
+                                try {
+                                    FileWriter writer = new FileWriter("./logs/failLoge.txt", true);
+                                    BufferedWriter bufferWriter = new BufferedWriter(writer);
+                                    bufferWriter.write("Date: " + item.message().date() + " text:" + item.message().text() + " chat:" + item.message().chat() + "\n");
+                                    bufferWriter.close();
+                                } catch( Exception ex){
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                    catch(IOException ex){
+
+                        System.out.println(ex.getMessage());
+                    }
+                }
+                return UpdatesListener.CONFIRMED_UPDATES_ALL;
+            }
+        }, new GetUpdates());
         SetWebhook wh = new SetWebhook();
         wh.url("https://cryptic-journey-87140.herokuapp.com/ ");
         bot.execute(wh, new Callback<SetWebhook, BaseResponse>() {
