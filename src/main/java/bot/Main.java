8783a26344455d02e62dcd6b9bf3143a6d6c6c60
Dmitry <elegiacal.boy@gmail.com>
2018-03-14 17:09:42 +0200

    simple echo bot with logs+webhook

--- src/main/java/bot/Main.java
+++ src/main/java/bot/Main.java
@@ -23,60 +23,60 @@ public class Main {
 
     public static void main(String[] args) {
         final TelegramBot bot = new TelegramBot.Builder("521960020:AAHGQKGlW_QbuedUGikDDcfagOMqbMYguI4").okHttpClient(new OkHttpClient()).build();
-        UpdatesHandler uh = new UpdatesHandler(1);
-        uh.start(bot,new UpdatesListener() {
-            public int process(List<Update> list) {
-                for(final Update item: list){
-                    try
-                    {
-                        FileWriter writer = new FileWriter("./logs/mainLogFile.txt", true);
-                        BufferedWriter bufferWriter = new BufferedWriter(writer);
-                        bufferWriter.write("Date: "+item.message().date() +" text:"+item.message().text()+" chat:"+item.message().chat()+"\n");
-                        bufferWriter.close();
-                        SendMessage sm = new SendMessage(item.message().chat().id(), item.message().text());
-                        bot.execute(sm, new Callback<SendMessage, SendResponse>() {
-                            public void onResponse(SendMessage sendMessage, SendResponse sendResponse) {
-
-                            }
-
-                            public void onFailure(SendMessage sendMessage, IOException e) {
-                                try {
-                                    FileWriter writer = new FileWriter("./logs/failLoge.txt", true);
-                                    BufferedWriter bufferWriter = new BufferedWriter(writer);
-                                    bufferWriter.write("Date: " + item.message().date() + " text:" + item.message().text() + " chat:" + item.message().chat() + "\n");
-                                    bufferWriter.close();
-                                } catch( Exception ex){
-                                    ex.printStackTrace();
-                                }
-                            }
-                        });
-                    }
-                    catch(IOException ex){
-
-                        System.out.println(ex.getMessage());
-                    }
-                }
-                return UpdatesListener.CONFIRMED_UPDATES_ALL;
-            }
-        }, new GetUpdates());
-        SetWebhook wh = new SetWebhook();
-        wh.url("https://cryptic-journey-87140.herokuapp.com/ ");
-        bot.execute(wh, new Callback<SetWebhook, BaseResponse>() {
-            public void onResponse(SetWebhook setWebhook, BaseResponse baseResponse) {
-
-            }
-
-            public void onFailure(SetWebhook setWebhook, IOException e) {
-                try {
-                    FileWriter writer = new FileWriter("./logs/failLoge.txt", true);
-                    BufferedWriter bufferWriter = new BufferedWriter(writer);
-                    bufferWriter.write("fail, while try set webhook");
-                    bufferWriter.close();
-                } catch( Exception ex){
-                    ex.printStackTrace();
-                }
-            }
-        });
+//        UpdatesHandler uh = new UpdatesHandler(1);
+//        uh.start(bot,new UpdatesListener() {
+//            public int process(List<Update> list) {
+//                for(final Update item: list){
+//                    try
+//                    {
+//                        FileWriter writer = new FileWriter("./logs/mainLogFile.txt", true);
+//                        BufferedWriter bufferWriter = new BufferedWriter(writer);
+//                        bufferWriter.write("Date: "+item.message().date() +" text:"+item.message().text()+" chat:"+item.message().chat()+"\n");
+//                        bufferWriter.close();
+//                        SendMessage sm = new SendMessage(item.message().chat().id(), item.message().text());
+//                        bot.execute(sm, new Callback<SendMessage, SendResponse>() {
+//                            public void onResponse(SendMessage sendMessage, SendResponse sendResponse) {
+//
+//                            }
+//
+//                            public void onFailure(SendMessage sendMessage, IOException e) {
+//                                try {
+//                                    FileWriter writer = new FileWriter("./logs/failLoge.txt", true);
+//                                    BufferedWriter bufferWriter = new BufferedWriter(writer);
+//                                    bufferWriter.write("Date: " + item.message().date() + " text:" + item.message().text() + " chat:" + item.message().chat() + "\n");
+//                                    bufferWriter.close();
+//                                } catch( Exception ex){
+//                                    ex.printStackTrace();
+//                                }
+//                            }
+//                        });
+//                    }
+//                    catch(IOException ex){
+//
+//                        System.out.println(ex.getMessage());
+//                    }
+//                }
+//                return UpdatesListener.CONFIRMED_UPDATES_ALL;
+//            }
+//        }, new GetUpdates());
+//        SetWebhook wh = new SetWebhook();
+//        wh.url("https://cryptic-journey-87140.herokuapp.com/ ");
+//        bot.execute(wh, new Callback<SetWebhook, BaseResponse>() {
+//            public void onResponse(SetWebhook setWebhook, BaseResponse baseResponse) {
+//
+//            }
+//
+//            public void onFailure(SetWebhook setWebhook, IOException e) {
+//                try {
+//                    FileWriter writer = new FileWriter("./logs/failLoge.txt", true);
+//                    BufferedWriter bufferWriter = new BufferedWriter(writer);
+//                    bufferWriter.write("fail, while try set webhook");
+//                    bufferWriter.close();
+//                } catch( Exception ex){
+//                    ex.printStackTrace();
+//                }
+//            }
+//        });
         bot.setUpdatesListener(new UpdatesListener() {
             public int process(List<Update> list) {
                 for(final Update item: list){
